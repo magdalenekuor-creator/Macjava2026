34bd65a04780ff6d34e2f59ec3345ca2ad23555b
magdalenekuor-creator <magdalenekuor@gmail.com>
2026-06-07 18:10:19 +0000

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -2,7 +2,29 @@ public class Hello{
 
     public static void main(String[] args){
 
-      System.out.print("Hello World");
+    String fullname   = "Magdalene Mangorkuor Teye";
+    int age           = 20;
+    char gender       = 'F';
+    double height     = 1.75;
+    float weight      = 68.5f;
+    boolean isStudent = true;
+    long phoneNumber  = 233559247685;
+    String grade      = "Level 200";
+    String major      = "Computer Science";
+    double gpa        = 4.05;
+
+
+    System.out.println("Full Name     : " + fullname);
+    System.out.println("Age           : " + age + " years"); 
+    System.out.println("Gender        : " + gender);
+    System.out.println("Height        : " + height + " m ");
+    Sy stem.out.println("Weight        : " + weight + " kg ");
+    System.out.println("Students      : " + isStudent);
+    System.out.println("Phone Number  : " + phoneNumber);
+    System.out.println("Grade/Level   : " + grade);
+    System.out.println("Major         : " + major);
+    System.out.println("GPA           : " + gpa);
+
 
     }
 
